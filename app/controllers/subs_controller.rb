57b8e9876e42c760f2822b7a0838309f1f08dc19
takuya178 <takuya.u1005@gmail.com>
2021-11-01 12:42:46 +0900

    メインとサブ料理のデザイン変更

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -3,4 +3,8 @@ class SubsController < ApplicationController
     @q = Sub.ransack(params[:q])
     @subs = @q.result(distinct: true).all.page(params[:page]).per(15)
   end
+
+  def show
+    @sub = Sub.find(params[:id])
+  end
 end
