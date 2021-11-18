cd0b1467610b16a25ce4bdcd3d3d38ee69da7f6b
takuya178 <takuya.u1005@gmail.com>
2021-11-18 14:58:33 +0900

    サブのみvalidateがかかる

--- app/forms/main_sub_form.rb
+++ app/forms/main_sub_form.rb
@@ -10,14 +10,6 @@ class MainSubForm
 
   attr_accessor :name, :image, :stores, :genre, :calorie, :sugar, :lipid, :salt
 
-  # メインのみvalidateかかる
-  validates :name, presence: true
-  validates :calorie, presence: true
-  validates :sugar, presence: true
-  validates :lipid, presence: true
-  validates :salt, presence: true
-
-
   concerning :SubBuilder do
     attr_reader :subs_attributes
 
