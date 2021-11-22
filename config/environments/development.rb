aef9edc23f79c3ced8f9da7ef4ff1a0f30a86fe0
takuya178 <takuya.u1005@gmail.com>
2021-11-22 16:15:52 +0900

    画像アップロード修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,7 @@ Rails.application.configure do
   config.assets.compile = true
   # Show full error reports.
   config.consider_all_requests_local = false
+  config.autoloader = :classic
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
