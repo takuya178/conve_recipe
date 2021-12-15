2beb0d3dd413d083c97f0b8423360bd5ae3c0125
takuya178 <takuya.u1005@gmail.com>
2021-12-15 17:32:31 +0900

    componentId不要だったので削除

--- app/controllers/food_combinations_controller.rb
+++ app/controllers/food_combinations_controller.rb
@@ -12,17 +12,16 @@ class FoodCombinationsController < ApplicationController
   private
 
   def set_food_combination
-    component = params[:componentId]
+    @genre_params = params[:genre]
     @component_params = params[:component]
-    genre_params = params[:genre]
-    component_ids = Tag.where(id: @component_params)
-    genre_ids = Tag.where(id: genre_params)
+    @component_ids = Tag.where(id: @component_params)
+    @genre_ids = Tag.where(id: @genre_params)
 
-    @sugar = Tag.find_by(id: component_ids, component: 'sugar')
-    @lipid = Tag.find_by(id: component_ids, component: 'lipid')
-    @salt = Tag.find_by(id: component_ids, component: 'salt')
-    @noodle = Tag.find_by(id: genre_ids, genre: 'noodle')
-    @rice = Tag.find_by(id: genre_ids, genre: 'rice')
+    @sugar = Tag.find_by(id: @component_ids, component: 'sugar')
+    @lipid = Tag.find_by(id: @component_ids, component: 'lipid')
+    @salt = Tag.find_by(id: @component_ids, component: 'salt')
+    @noodle = Tag.find_by(id: @genre_ids, genre: 'noodle')
+    @rice = Tag.find_by(id: @genre_ids, genre: 'rice')
 
     @pagy, @foods = pagy(FoodCombination.preload(:main, :sub).preload_food_image, items: 25)
   end
