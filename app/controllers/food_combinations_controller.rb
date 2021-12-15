7896063c8bc72733eb721413630bb79fa565ff5c
takuya178 <takuya.u1005@gmail.com>
2021-12-15 18:33:04 +0900

    if文にgenreを加えることでSQL発光数少なくする

--- app/controllers/food_combinations_controller.rb
+++ app/controllers/food_combinations_controller.rb
@@ -17,38 +17,57 @@ class FoodCombinationsController < ApplicationController
     component_ids = Tag.where(id: @component_params)
     genre_ids = Tag.where(id: genre_params)
 
-    if @component_params == ['1']
+    if @component_params == ['1'] && genre_params == ['4']
       @sugar = Tag.find_by(id: component_ids, component: 'sugar')
       @noodle = Tag.find_by(id: genre_ids, genre: 'noodle')
+    elsif @component_params == ['1'] && genre_params == ['5']
+      @sugar = Tag.find_by(id: component_ids, component: 'sugar')
       @rice = Tag.find_by(id: genre_ids, genre: 'rice')
-    elsif @component_params == ['2']
+    elsif @component_params == ['2'] && genre_params == ['4']
       @lipid = Tag.find_by(id: component_ids, component: 'lipid')
       @noodle = Tag.find_by(id: genre_ids, genre: 'noodle')
+    elsif @component_params == ['2'] && genre_params == ['5']
+      @lipid = Tag.find_by(id: component_ids, component: 'lipid')
       @rice = Tag.find_by(id: genre_ids, genre: 'rice')
-    elsif @component_params == ['3']
+    elsif @component_params == ['3'] && genre_params == ['4']
       @salt = Tag.find_by(id: component_ids, component: 'salt')
       @noodle = Tag.find_by(id: genre_ids, genre: 'noodle')
+    elsif @component_params == ['3'] && genre_params == ['5']
+      @salt = Tag.find_by(id: component_ids, component: 'salt')
       @rice = Tag.find_by(id: genre_ids, genre: 'rice')
-    elsif @component_params == ['1', '2']
+    elsif @component_params == ['1', '2'] && genre_params == ['4']
       @sugar = Tag.find_by(id: component_ids, component: 'sugar')
       @lipid = Tag.find_by(id: component_ids, component: 'lipid')
       @noodle = Tag.find_by(id: genre_ids, genre: 'noodle')
+    elsif @component_params == ['1', '2'] && genre_params == ['5']
+      @sugar = Tag.find_by(id: component_ids, component: 'sugar')
+      @lipid = Tag.find_by(id: component_ids, component: 'lipid')
       @rice = Tag.find_by(id: genre_ids, genre: 'rice')
-    elsif @component_params == ['1', '3']
+    elsif @component_params == ['1', '3'] && genre_params == ['4']
       @sugar = Tag.find_by(id: component_ids, component: 'sugar')
       @salt = Tag.find_by(id: component_ids, component: 'salt')
       @noodle = Tag.find_by(id: genre_ids, genre: 'noodle')
+    elsif @component_params == ['1', '3'] && genre_params == ['5']
+      @sugar = Tag.find_by(id: component_ids, component: 'sugar')
+      @salt = Tag.find_by(id: component_ids, component: 'salt')
       @rice = Tag.find_by(id: genre_ids, genre: 'rice')
-    elsif @component_params == ['2', '3']
+    elsif @component_params == ['2', '3'] && genre_params == ['4']
       @lipid = Tag.find_by(id: component_ids, component: 'lipid')
       @salt = Tag.find_by(id: component_ids, component: 'salt')
       @noodle = Tag.find_by(id: genre_ids, genre: 'noodle')
+    elsif @component_params == ['2', '3'] && genre_params == ['5']
+      @lipid = Tag.find_by(id: component_ids, component: 'lipid')
+      @salt = Tag.find_by(id: component_ids, component: 'salt')
       @rice = Tag.find_by(id: genre_ids, genre: 'rice')
-    elsif @component_params == ['1', '2', '3']
+    elsif @component_params == ['1', '2', '3'] && genre_params == ['4']
       @sugar = Tag.find_by(id: component_ids, component: 'sugar')
       @lipid = Tag.find_by(id: component_ids, component: 'lipid')
       @salt = Tag.find_by(id: component_ids, component: 'salt')
       @noodle = Tag.find_by(id: genre_ids, genre: 'noodle')
+    elsif @component_params == ['1', '2', '3'] && genre_params == ['5']
+      @sugar = Tag.find_by(id: component_ids, component: 'sugar')
+      @lipid = Tag.find_by(id: component_ids, component: 'lipid')
+      @salt = Tag.find_by(id: component_ids, component: 'salt')
       @rice = Tag.find_by(id: genre_ids, genre: 'rice')
     end
 
