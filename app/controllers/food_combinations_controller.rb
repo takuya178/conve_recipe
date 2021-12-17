8941b29269e706ffaab269086924b1b00ad12521
takuya178 <takuya.u1005@gmail.com>
2021-12-17 20:37:58 +0900

    scopeで省略

--- app/controllers/food_combinations_controller.rb
+++ app/controllers/food_combinations_controller.rb
@@ -14,61 +14,59 @@ class FoodCombinationsController < ApplicationController
   def set_food_combination
     @component_params = params[:component]
     genre_params = params[:genre]
-    component_ids = Tag.where(id: @component_params)
-    genre_ids = Tag.where(id: genre_params)
 
     if @component_params == ['1'] && genre_params == ['4']
-      @sugar = Tag.where(id: component_ids, component: 'sugar')
-      @noodle = Tag.where(id: genre_ids, genre: 'noodle')
+      @sugar = Tag.where_component(1, 'sugar')
+      @noodle = Tag.where_genre(4, 'noodle')
     elsif @component_params == ['1'] && genre_params == ['5']
-      @sugar = Tag.where(id: component_ids, component: 'sugar')
-      @rice = Tag.where(id: genre_ids, genre: 'rice')
+      @sugar = Tag.where_component(1, 'sugar')
+      @rice = Tag.where_genre(5, 'rice')
     elsif @component_params == ['2'] && genre_params == ['4']
-      @lipid = Tag.where(id: component_ids, component: 'lipid')
-      @noodle = Tag.where(id: genre_ids, genre: 'noodle')
+      @lipid = Tag.where_component(2, 'lipid')
+      @noodle = Tag.where_genre(4, 'noodle')
     elsif @component_params == ['2'] && genre_params == ['5']
-      @lipid = Tag.where(id: component_ids, component: 'lipid')
-      @rice = Tag.where(id: genre_ids, genre: 'rice')
+      @lipid = Tag.where_component(2, 'lipid')
+      @rice = Tag.where_genre(5, 'rice')
     elsif @component_params == ['3'] && genre_params == ['4']
-      @salt = Tag.where(id: component_ids, component: 'salt')
-      @noodle = Tag.where(id: genre_ids, genre: 'noodle')
+      @salt = Tag.where_component(3, 'salt')
+      @noodle = Tag.where_genre(4, 'noodle')
     elsif @component_params == ['3'] && genre_params == ['5']
-      @salt = Tag.where(id: component_ids, component: 'salt')
-      @rice = Tag.where(id: genre_ids, genre: 'rice')
+      @salt = Tag.where_component(3, 'salt')
+      @rice = Tag.where_genre(5, 'rice')
     elsif @component_params == ['1', '2'] && genre_params == ['4']
-      @sugar = Tag.where(id: component_ids, component: 'sugar')
-      @lipid = Tag.where(id: component_ids, component: 'lipid')
-      @noodle = Tag.where(id: genre_ids, genre: 'noodle')
+      @sugar = Tag.where_component(1, 'sugar')
+      @lipid = Tag.where_component(2, 'lipid')
+      @noodle = Tag.where_genre(4, 'noodle')
     elsif @component_params == ['1', '2'] && genre_params == ['5']
-      @sugar = Tag.where(id: component_ids, component: 'sugar')
-      @lipid = Tag.where(id: component_ids, component: 'lipid')
-      @rice = Tag.where(id: genre_ids, genre: 'rice')
+      @sugar = Tag.where_component(1, 'sugar')
+      @lipid = Tag.where_component(2, 'lipid')
+      @rice = Tag.where_genre(5, 'rice')
     elsif @component_params == ['1', '3'] && genre_params == ['4']
-      @sugar = Tag.where(id: component_ids, component: 'sugar')
-      @salt = Tag.where(id: component_ids, component: 'salt')
-      @noodle = Tag.where(id: genre_ids, genre: 'noodle')
+      @sugar = Tag.where_component(1, 'sugar')
+      @salt = Tag.where_component(3, 'salt')
+      @noodle = Tag.where_genre(4, 'noodle')
     elsif @component_params == ['1', '3'] && genre_params == ['5']
-      @sugar = Tag.where(id: component_ids, component: 'sugar')
-      @salt = Tag.where(id: component_ids, component: 'salt')
-      @rice = Tag.where(id: genre_ids, genre: 'rice')
+      @sugar = Tag.where_component(1, 'sugar')
+      @salt = Tag.where_component(3, 'salt')
+      @rice = Tag.where_genre(5, 'rice')
     elsif @component_params == ['2', '3'] && genre_params == ['4']
-      @lipid = Tag.where(id: component_ids, component: 'lipid')
-      @salt = Tag.where(id: component_ids, component: 'salt')
-      @noodle = Tag.where(id: genre_ids, genre: 'noodle')
+      @lipid = Tag.where_component(2, 'lipid')
+      @salt = Tag.where_component(3, 'salt')
+      @noodle = Tag.where_genre(4, 'noodle')
     elsif @component_params == ['2', '3'] && genre_params == ['5']
-      @lipid = Tag.where(id: component_ids, component: 'lipid')
-      @salt = Tag.where(id: component_ids, component: 'salt')
-      @rice = Tag.where(id: genre_ids, genre: 'rice')
+      @lipid = Tag.where_component(2, 'lipid')
+      @salt = Tag.where_component(3, 'salt')
+      @rice = Tag.where_genre(5, 'rice')
     elsif @component_params == ['1', '2', '3'] && genre_params == ['4']
-      @sugar = Tag.where(id: component_ids, component: 'sugar')
-      @lipid = Tag.where(id: component_ids, component: 'lipid')
-      @salt = Tag.where(id: component_ids, component: 'salt')
-      @noodle = Tag.where(id: genre_ids, genre: 'noodle')
+      @sugar = Tag.where_component(1, 'sugar')
+      @lipid = Tag.where_component(2, 'lipid')
+      @salt = Tag.where_component(3, 'salt')
+      @noodle = Tag.where_genre(4, 'noodle')
     elsif @component_params == ['1', '2', '3'] && genre_params == ['5']
-      @sugar = Tag.where(id: component_ids, component: 'sugar')
-      @lipid = Tag.where(id: component_ids, component: 'lipid')
-      @salt = Tag.where(id: component_ids, component: 'salt')
-      @rice = Tag.where(id: genre_ids, genre: 'rice')
+      @sugar = Tag.where_component(1, 'sugar')
+      @lipid = Tag.where_component(2, 'lipid')
+      @salt = Tag.where_component(3, 'salt')
+      @rice = Tag.where_genre(5, 'rice')
     end
 
     @pagy, @foods = pagy(FoodCombination.preload(:main, :sub).preload_food_image, items: 25)
