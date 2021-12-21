154592ebb879696cd4b621d9f577974079ea952f
takuya178 <takuya.u1005@gmail.com>
2021-12-21 14:45:52 +0900

    サブ料理の画像を保存できるようにcontroller修正

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -3,7 +3,7 @@ class MainsController < ApplicationController
 
   def index
     @q = Main.ransack(params[:q])
-    @pagy, @mains = pagy(@q.result(distinct: true).eager_loaded_main_image, items: 14)
+    @pagy, @mains = pagy(@q.result(distinct: true).preload_main_image, items: 14)
   end
 
   def show
@@ -17,9 +17,9 @@ class MainsController < ApplicationController
 
   def create
     @main_sub_form = MainSubForm.new(main_sub_form_params)
-    main_data = Main.find_or_initialize_by(name: main_sub_form_params[:name])
+    main_initialize = Main.find_or_initialize_by(name: main_sub_form_params[:name])
 
-    if main_data.new_record?
+    if main_initialize.new_record?
       if @main_sub_form.save
         redirect_to new_main_path, success: '組み合わせを追加しました'
       else
@@ -27,9 +27,9 @@ class MainsController < ApplicationController
         render :new
       end
     else
-      main_data.subs << Sub.create(name: main_sub_form_params[:subs_attributes][:name], calorie: main_sub_form_params[:subs_attributes][:calorie], sugar: main_sub_form_params[:subs_attributes][:sugar], lipid: main_sub_form_params[:subs_attributes][:lipid], salt: main_sub_form_params[:subs_attributes][:salt], stores: main_sub_form_params[:subs_attributes][:stores])
+      main_initialize.subs << Sub.create(image: main_sub_form_params[:subs_attributes][:image], name: main_sub_form_params[:subs_attributes][:name], calorie: main_sub_form_params[:subs_attributes][:calorie], sugar: main_sub_form_params[:subs_attributes][:sugar], lipid: main_sub_form_params[:subs_attributes][:lipid], salt: main_sub_form_params[:subs_attributes][:salt], stores: main_sub_form_params[:subs_attributes][:stores])
 
-      redirect_to new_main_path, success: "#{main_data.name}に新しい組み合わせを追加しました"
+      redirect_to new_main_path, success: "#{main_initialize.name}に新しい組み合わせを追加しました"
     end
   end
 
