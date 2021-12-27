832787848f56844a0f38623571b3404c31bd840a
takuya178 <takuya.u1005@gmail.com>
2021-12-27 11:35:57 +0900

    description追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,24 +15,22 @@ module ApplicationHelper
       separator: '|',
       description: 'コンビニ食品を麺類・ご飯類の中から食べたいジャンルを選んでいただき、それに合う一品料理と摂取を控えたい成分が表示されます。お好みの組み合わせを作れるので皆と共有しよう！',
       keywords: 'healthy-combi, 成分, コンビニ 組み合わせ, ',
-      canonical: request.original_url,
+      canonical: 'https://www.healthy-combi.com',
       icon: [
-        { href: image_url('favicon.ico') },
-        { href: image_url('ogp_apple_touch_icon.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' }
+        { href: asset_pack_url('media/images/meta_tag_top.jpg') },
       ],
       og: {
         site_name: :site,
         title: :title,
         description: :description,
         type: 'website',
-        url: request.original_url,
-        image: image_url('ogp_twitter_card.png'),
+        url: :canonical,
+        image: asset_pack_url('media/images/meta_tag_top.jpg'),
         locale: 'ja_JP'
       },
       twitter: {
-        site: '@st3110123',
-        card: 'summary_large_image',
-        image: image_url('ogp_twitter_card.png')
+        site: '@j_u105',
+        card: 'summary_large_image'
       }
     }
   end
