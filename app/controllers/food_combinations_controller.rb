ec2bb37df8d52df60bdb1f5f73632deb4d0b7f6d
takuya178 <takuya.u1005@gmail.com>
2022-01-10 21:58:42 +0900

    成分が全て表示されていたので修正

--- app/controllers/food_combinations_controller.rb
+++ app/controllers/food_combinations_controller.rb
@@ -14,12 +14,14 @@ class FoodCombinationsController < ApplicationController
   def set_food_combination
     @component_params = params[:component]
     genre_params = params[:genre]
+    component_ids = Tag.where(id: @component_params)
+    genre_ids = Tag.where(id: genre_params)
 
-    @sugar = Tag.find_component(1, 'sugar')
-    @lipid = Tag.find_component(2, 'lipid')
-    @salt = Tag.find_component(3, 'salt')
-    @noodle = Tag.find_genre(4, 'noodle')
-    @rice = Tag.find_genre(5, 'rice')
+    @sugar = Tag.find_by(id: component_ids, component: 'sugar')
+    @lipid = Tag.find_by(id: component_ids, component: 'lipid')
+    @salt = Tag.find_by(id: component_ids, component: 'salt')
+    @noodle = Tag.find_by(id: genre_ids, genre: 'noodle')
+    @rice = Tag.find_by(id: genre_ids, genre: 'rice')
 
     @pagy, @foods = pagy(FoodCombination.preload(:main, :sub).preload_food_image, items: 25)
   end
